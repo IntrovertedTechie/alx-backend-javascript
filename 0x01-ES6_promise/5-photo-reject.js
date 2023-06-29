257c5035a004cc0b09b1f9f0ac36600d3cf9f476
Introvertedtechie <zillionaire234@gmail.com>
2023-06-29 11:42:13 +0000

    promise

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,4 @@
-//The code exports a function named uploadPhoto that returns a rejected promise with an error message stating that the provided fileName cannot be processed.
-
+//The code exports a function named uploadPhoto
 export default function uploadPhoto(fileName) {
   return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
-
-
