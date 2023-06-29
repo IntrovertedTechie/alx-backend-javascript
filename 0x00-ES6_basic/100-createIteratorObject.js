d93ade8e7290227b50c51f86becf5f58fb56d1c7
Introvertedtechie <zillionaire234@gmail.com>
2023-06-29 15:05:20 +0000

    promise yoi no dey celebrate sallah

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,35 @@
+
+export default function createIteratorObject(report) {
+  const departments = Object.keys(report.allEmployees);
+
+  const iterator = {
+    [Symbol.iterator]() {
+      let departmentIndex = 0;
+      let employeeIndex = 0;
+
+      return {
+        next() {
+          if (departmentIndex < departments.length) {
+            const department = departments[departmentIndex];
+            const employees = report.allEmployees[department];
+
+            if (employeeIndex < employees.length) {
+              const value = employees[employeeIndex];
+              employeeIndex++;
+              return { value, done: false };
+            } else {
+              departmentIndex++;
+              employeeIndex = 0;
+              return this.next();
+            }
+          } else {
+            return { done: true };
+          }
+        },
+      };
+    },
+  };
+
+  return iterator;
+}
+
