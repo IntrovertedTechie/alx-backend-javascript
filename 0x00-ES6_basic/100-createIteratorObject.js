8a90b4bcb77242f8e658e42fa4a46b6df79aa032
Introvertedtechie <zillionaire234@gmail.com>
2023-06-30 14:33:50 +0000

    pro

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -32,4 +32,3 @@ export default function createIteratorObject(report) {
 
   return iterator;
 }
-
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -10,4 +10,3 @@ export default function iterateThroughObject(reportWithIterator) {
 
   return result;
 }
-
