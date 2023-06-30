229d494de4525b827eb384ed093a130a90c9d9d7
Adio Adebayo <zillionaire234@gmail.com>
2023-06-30 21:20:51 +0100

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,15 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
+    this.students = [];
+  }
+
+  addStudent(student) {
+    if (this.students.length < this._maxStudentsSize) {
+      this.students.push(student);
+      console.log(`${student} added to the class.`);
+    } else {
+      console.log(`The class is already full. Cannot add ${student}.`);
+    }
   }
 }
-
