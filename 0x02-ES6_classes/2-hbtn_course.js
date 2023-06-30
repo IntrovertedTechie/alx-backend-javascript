5f28c7abd092fb2d9c723f04539ab78b255597c7
Adio Adebayo <zillionaire234@gmail.com>
2023-06-30 22:07:20 +0100

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,55 +1,54 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateString(name, 'Name');
-    this._length = this.validateNumber(length, 'Length');
-    this._students = this.validateArray(students, 'Students');
+    this._name = this.validateString(name, 'Name')
+    this._length = this.validateNumber(length, 'Length')
+    this._students = this.validateArray(students, 'Students')
   }
 
   get name() {
-    return this._name;
+    return this._name
   }
 
   set name(newName) {
-    this._name = this.validateString(newName, 'Name');
+    this._name = this.validateString(newName, 'Name')
   }
 
   get length() {
-    return this._length;
+    return this._length
   }
 
   set length(newLength) {
-    this._length = this.validateNumber(newLength, 'Length');
+    this._length = this.validateNumber(newLength, 'Length')
   }
 
   get students() {
-    return this._students;
+    return this._students
   }
 
   set students(newStudents) {
-    this._students = this.validateArray(newStudents, 'Students');
+    this._students = this.validateArray(newStudents, 'Students')
   }
 
-  validateString(value, propertyName) {
+  validateString (value, propertyName) {
     if (typeof value !== 'string') {
-      throw new TypeError(`${propertyName} must be a string`);
+      throw new TypeError(`${propertyName} must be a string`)
     }
-    return value;
+    return value
   }
 
-  validateNumber(value, propertyName) {
+  validateNumber (value, propertyName) {
     if (typeof value !== 'number') {
-      throw new TypeError(`${propertyName} must be a number`);
+      throw new TypeError(`${propertyName} must be a number`)
     }
-    return value;
+    return value
   }
 
-  validateArray(value, propertyName) {
+  validateArray (value, propertyName) {
     if (!Array.isArray(value)) {
-      throw new TypeError(`${propertyName} must be an array`);
+      throw new TypeError(`${propertyName} must be an array`)
     }
-    return value;
+    return value
   }
 }
 
 export default HolbertonCourse;
-
