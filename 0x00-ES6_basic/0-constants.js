fc922d12cd01c7311d8a309d3bbdcc0f13017961
Introvertedtechie <zillionaire234@gmail.com>
2023-06-30 13:43:02 +0000

    pro

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,16 @@
-// Returns the string 'I prefer const when I can.'
+
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Declare a constant variable 'task'
+  const task = 'I prefer const when I can.';
   return task;
 }
 
-// Returns the string ' is okay'
 export function getLast() {
   return ' is okay';
 }
 
-// Returns a combination of two strings
 export function taskNext() {
-  let combination = 'But sometimes let'; // Declare a variable 'combination' using 'let'
-  combination += getLast(); // Append the result of getLast() to 'combination'
+  let combination = 'But sometimes let';
+  combination += getLast();
 
   return combination;
 }
