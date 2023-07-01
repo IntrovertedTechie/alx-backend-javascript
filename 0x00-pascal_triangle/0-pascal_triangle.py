aed9d22d9e16a38d78d275640c334384252ca73a
Adio Adebayo <zillionaire234@gmail.com>
2023-07-01 18:23:33 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,6 @@
  print_triangle(triangle):
     for row in triangle:
-       """" Print_triangle""" print("[{}]".format(",".join([str(x) for x in row])))
+
+ print("[{}]".format(",".join([(x) for x in row])))
 
 print_triangle(pascal_triangle(5))
