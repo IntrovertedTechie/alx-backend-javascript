04b3b137f0285e97bb857a8ad7cbaede9103d79f
Introvertedtechie <zillionaire234@gmail.com>
2023-07-03 10:49:34 +0100

    data manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+function cleanSet(set, startString) {
+  const values = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      values.push(value.substring(startString.length));
+    }
+  }
+  return values.join("-");
+}
+
+module.exports = cleanSet;
