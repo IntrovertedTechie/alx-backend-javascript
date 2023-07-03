e4fadaa4a327b9644c4a117539ddfb78ece8c791
Adio Adebayo <zillionaire234@gmail.com>
2023-07-03 19:24:08 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,8 +2,9 @@ function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San  }
   ];
-}
+      return student ;
+};
 
 export default getListStudents;
