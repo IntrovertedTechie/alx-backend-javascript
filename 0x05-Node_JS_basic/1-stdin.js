b903dbe0e9ad156222e189b6b247bf815ee6ec82
Adio Adebayo <zillionaire234@gmail.com>
2023-08-22 11:44:47 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,9 @@ const process = require('process');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
-});
-
-process.stdin.on('end', () => {
+process.stdin.once('data', (data) => {
+  const name = data.toString().trim(); // Trim any whitespace or newline characters
+  process.stdout.write(`Your name is: ${name}\n`);
   process.stdout.write('This important software is now closing\n');
+  process.exit(0); // Exit the program
 });
