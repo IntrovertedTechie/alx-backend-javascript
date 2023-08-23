09a53cf797bba8b177b18af57743f7ba9b8a8744
Introvertedtechie <zillionaire234@gmail.com>
2023-08-23 17:04:00 +0000

    java

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('type == "SUM"', () => {
+    it('equal positive numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('type == "SUBTRACT"', () => {
+    it('equal positive numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });  
+  });
+
+  describe('type == "DIVIDE"', () => {
+    it('positive numbers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5),0.2);
+    });
+
+    it('positive number and 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+});
+});
