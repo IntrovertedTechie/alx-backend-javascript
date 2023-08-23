0094969945e2294a1d8ef29a50da304ecd9088bd
Adio Adebayo <zillionaire234@gmail.com>
2023-08-23 19:00:05 +0100

    Update 2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,26 +1,26 @@
-const assert = require('chai');
-const calculateNumber = require('./2-calcul_chai');
-
-describe('calculateNumber', () => {
-  describe('type == "SUM"', () => {
-    it('equal positive numbers', () => {
-      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-    });
-  });
-
-  describe('type == "SUBTRACT"', () => {
-    it('equal positive numbers', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-    });  
-  });
-
-  describe('type == "DIVIDE"', () => {
-    it('positive numbers', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5),0.2);
-    });
-
-    it('positive number and 0', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    });
-});
-});
+const expect = require('chai'); 
+ const calculateNumber = require('./2-calcul_chai'); 
+  
+ describe('calculateNumber', () => { 
+   describe('type == "SUM"', () => { 
+     it('equal positive numbers', () => { 
+       expect(calculateNumber('SUM', 1.4, 4.5), 6); 
+     }); 
+   }); 
+  
+   describe('type == "SUBTRACT"', () => { 
+     it('equal positive numbers', () => { 
+       expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4); 
+     });   
+   }); 
+  
+   describe('type == "DIVIDE"', () => { 
+     it('positive numbers', () => { 
+       expect(calculateNumber('DIVIDE', 1.4, 4.5),0.2); 
+     }); 
+  
+     it('positive number and 0', () => { 
+       expect(calculateNumber('DIVIDE', 1.4, 0), 'Error'); 
+     }); 
+ }); 
+ });
