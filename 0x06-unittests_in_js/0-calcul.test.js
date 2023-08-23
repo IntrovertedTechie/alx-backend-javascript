182deee586f40b0630b7af40a97bd1dbcbd234ec
Introvertedtechie <zillionaire234@gmail.com>
2023-08-23 16:24:18 +0000

    0x06. Unittests in JS

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('whole numbers', () => {
+    assert.strictEqual(calculateNumber(1, 2), 3);
+  });
+
+  it('it round whole  number', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('rounding down a and b whole and   fractional number', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('rounding down floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('rounding up floating point fractional numbers', () => {
+   
+}); 
+}); 
