4f15fcb911b62afb36ff10a8b659c13eb4c367f6
Adio Adebayo <zillionaire234@gmail.com>
2023-08-24 08:01:26 +0100

    Update api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 7865;
 
@@ -15,22 +15,24 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${id}`);
 });
 
-app.get('/available_payments', (_req, res) => {
-  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+app.use(bodyParser.json());
+
+app.get('/available_payments', (req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
 });
 
 app.post('/login', (req, res) => {
-  let username = '';
-
-  if (req.body) {
-    username = req.body.userName;
-  }
-
-  res.send(`Welcome ${username}`);
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
