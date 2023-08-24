2ab93c218dec87a661256e57ce6eb7cb97149fab
Adio Adebayo <zillionaire234@gmail.com>
2023-08-24 07:03:52 +0100

    Update api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -13,6 +13,17 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${id}`);
 });
 
+
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
