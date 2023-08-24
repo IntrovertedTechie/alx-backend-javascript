b0623480bd40221ba780239507d3484ddc61d770
Adio Adebayo <zillionaire234@gmail.com>
2023-08-24 08:53:47 +0100

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('whole numbers', () => {
+  it('handles whole numbers', () => {
     assert.strictEqual(calculateNumber(1, 2), 3);
   });
 
-  it('it round whole  number', () => {
+  it('rounds a whole number', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('rounding down a and b whole and   fractional number', () => {
+  it('rounds down with fractional numbers', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('rounding down floating point fractional number', () => {
+  it('rounds up with floating point fractional numbers', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-}); 
+});
